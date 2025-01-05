8a58e8f4c969b54b01f69a3d3eaa6bf111bb795e
Jhazeel Hernandez <140919471+Jhazeel-mmh@users.noreply.github.com>
2025-01-04 23:48:39 -0600

    add the getWeather function: fetches the data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./style.css";
 import "./normalize.css";
+
+const WEATHER_KEY = "FULVUHH7Y75BCMEVWAYCYS8MU";
+
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=us&key=${WEATHER_KEY}&contentType=json`,
+    );
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+getWeather("oaxaca")
+  .then((r) => console.log(r))
+  .catch((r) => console.log(r));
