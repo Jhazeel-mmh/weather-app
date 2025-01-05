73705ebe4b2a70badfcb56ab0c7920d3ecbe7245
Jhazeel Hernandez <140919471+Jhazeel-mmh@users.noreply.github.com>
2025-01-05 15:56:42 -0600

    Add displayInformation(obj)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import "./style.css";
 import "./normalize.css";
 
+const $ = (el) => document.querySelector(el);
+const c$ = (el, cls, content) => {
+  const elem = document.createElement(el);
+  elem.classList.add(cls);
+  elem.textContent = content;
+  return elem;
+};
+
 const WEATHER_KEY = "FULVUHH7Y75BCMEVWAYCYS8MU";
 
 async function getWeather(location) {
@@ -15,5 +23,26 @@ async function getWeather(location) {
 }
 
 getWeather("oaxaca")
-  .then((r) => console.log(r))
+  .then((r) => {
+    console.log(r);
+    displayInformation(r);
+  })
   .catch((r) => console.log(r));
+
+function displayInformation(obj) {
+  const container = $('.weather-container')
+  const { currentConditions, resolvedAddress, timezone, latitude, longitude, days } = obj
+  const day = days[0].datetime;
+  
+  container.innerHTML = `
+        <p class="city">${resolvedAddress}</p>
+        <p class="date">${day}</p>
+        <div class="current-conditions">
+            <p class="conditions">${currentConditions.conditions}</p>
+            <p class="temp">${currentConditions.temp}</p>
+        </div>
+        <p class="timezone">${timezone}</p>
+        <p class="latitude">${latitude}</p>
+        <p class="longitude">${longitude}</p>
+  `
+}
